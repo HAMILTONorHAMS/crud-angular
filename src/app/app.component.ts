0eff259bf5397605186735cf23611cf49c7b04b0
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2024-12-20 23:05:39 -0300

    updateGitIgnore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Crud-angular';
+  // title = 'Crud-angular';
+  url =
+    'https://www.receiteria.com.br/wp-content/uploads/receitas-de-massas-0.jpg';
+  largura = '200'
 }
